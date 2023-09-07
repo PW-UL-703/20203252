e9c69e7a8ccfba5147c3e9f015728e81ee549bbd
L34207 <L34207@lab.aca>
2023-09-06 19:42:25 -0500

    hola

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+function suma(){
+    let v1 = document.getElementById("op1").value 
+    let v2 = document.getElementById("op1").value
+
+    let rpta = parseInt(v1) + parseInt(v2)
+
+    document.getElementById("rpta").innerHTML = rpta
+}
